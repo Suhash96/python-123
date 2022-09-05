74301a98754b610a3b1ca6f8961e11877594ef6b
Suhash96 <112850474+Suhash96@users.noreply.github.com>
2022-09-05 13:29:27 +0530

    Add files via upload

---
+++ s1.py
@@ -0,0 +1,7 @@
+# Temperature in celsius degree
+celsius = 40
+fahrenheit = (celsius * 1.8) + 32
+
+# printing the result
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit'
+	%(celsius, fahrenheit))
