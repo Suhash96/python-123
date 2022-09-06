7ae36482e04e3dc8cce942f26d328710d4837cec
Suhash96 <112850474+Suhash96@users.noreply.github.com>
2022-09-06 12:12:19 +0530

    Add files via upload

---
+++ s21.py
@@ -0,0 +1,12 @@
+# program to find the grades and marks of the student using conditional statement
+marks=int(input( 'enter the marks'))
+if(marks>=90):
+    print('grade:A')
+
+elif(marks>=80):
+     print('grade:B')
+elif(marks>=70):
+     print('grade:C')
+else:
+     print('fail')
+    
