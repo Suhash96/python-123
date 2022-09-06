a156720dea0a283f8ee18b51b9de65d837afad70
Suhash96 <112850474+Suhash96@users.noreply.github.com>
2022-09-06 11:09:01 +0530

    Add files via upload

---
+++ test 1.py
@@ -0,0 +1,16 @@
+
+#To calculate simple interest for the given principle amount
+    
+def simple_interest(a,b,c):
+    print('The principal amount ', a)
+    print('The time taken ', b)
+    print('The rate of interest ',c)
+	
+    si = (a * b * c)/100
+	
+    print('The Simple Interest is', si)
+
+simple_interest(1000,2,12)
+
+    
+    
