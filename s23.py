49e88f5fda8a6ab9a0cc9c35c447dec64f3a0bf2
Suhash96 <112850474+Suhash96@users.noreply.github.com>
2022-09-06 13:31:47 +0530

    Add files via upload

---
+++ s23.py
@@ -0,0 +1,10 @@
+import cmath
+a = 8
+b = 9
+c = 10
+
+d = (b**2) - (4*a*c)
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1,sol2))
