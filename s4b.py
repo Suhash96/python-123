67e53634c1d8e1b283e24f04b43f81200c9a6ffb
Suhash96 <112850474+Suhash96@users.noreply.github.com>
2022-09-07 12:34:26 +0530

    Update s4b.py

--- s4b.py
+++ s4b.py
@@ -2,6 +2,6 @@
 
 n = int(input("Enter a number: "))
 if (n % 2) == 0:
-   print("{0} is Even".n)
+   print("{0} is Even".format(n))
 else:
-   print("{0} is Odd". n)
+   print("{0} is Odd".format (n))
