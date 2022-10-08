397db7897f7a43bd2a826a9458dcca2d79138150
Suhash96 <112850474+Suhash96@users.noreply.github.com>
2022-10-08 10:21:04 +0530

    Add files via upload

---
+++ day 5 lengthoflastwords.py
@@ -0,0 +1,17 @@
+def lengthOfLastWord(a):
+	l = 0
+
+
+	x = a.strip()
+
+	for i in range(len(x)):
+		if x[i] == " ":
+			l = 0
+		else:
+			l += 1
+	return l
+
+
+if __name__ == "__main__":
+	b = str(input("enter the words"))
+	print("The length of last word is",lengthOfLastWord(b))
